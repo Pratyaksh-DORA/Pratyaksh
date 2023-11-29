51a5526b4c349c2555ccae83b30c56b8558b69d2
heisdinesh <dinesh35382@gmail.com>
2023-11-29 17:13:15 +0530

    Docs - updated readme

--- readme.md
+++ readme.md
@@ -71,9 +71,9 @@ Here's a brief overview of how to use FLOW:
 We welcome contributions! To contribute to FLOW, follow these steps:
 
 1. Fork the repository.
-2. Create a new branch: git checkout -b feature/your-feature.
-3. Commit your changes: git commit -m 'Add some feature'.
-4. Push to the branch: git push origin feature/your-feature.
+2. Create a new branch: `git checkout -b feature/your-feature`.
+3. Commit your changes: `git commit -m 'Add some feature'`.
+4. Push to the branch: `git push origin feature/your-feature`.
 5. Open a pull request.
 
 ## License
--- server/routes/project.js
+++ server/routes/project.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { addProject, getOneProject, getAllProjectOfUser, getAllUsersOfProject } = require("../controllers/projectController");
 const { isLoggedIn, customRole } = require("../middlewares/user")
 
-router.route("/addProject").post(isLoggedIn, customRole("projectManager"), addProject);
+router.route("/addProject").post(isLoggedIn, customRole("projectManager", "admin"), addProject);
 router.route("/getOneProject/:id").get(isLoggedIn, getOneProject);
 router.route("/getAllProjectOfUser").get(isLoggedIn, getAllProjectOfUser);
 router.route("/getAllUsersOfProject").get(isLoggedIn, getAllUsersOfProject);
