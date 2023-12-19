59dcca83f3897f2f6b577301a6a2291e521c4aa3
Ishika Jain <ishika2413@gmail.com>
2023-12-19 09:36:21 +0530

    FD

--- client/src/pages/GIS.js
+++ client/src/pages/GIS.js
@@ -1,64 +1,72 @@
-import React, {useEffect, useState} from 'react';
-import { MapContainer, TileLayer, useMap, Marker, Popup, ZoomControl } from 'react-leaflet'
+import React, { useEffect, useState } from "react";
+import {
+  MapContainer,
+  TileLayer,
+  useMap,
+  Marker,
+  Popup,
+  ZoomControl,
+} from "react-leaflet";
 import { Icon, divIcon, point } from "leaflet";
-import 'leaflet/dist/leaflet.css';
+import "leaflet/dist/leaflet.css";
 import { RiExternalLinkLine } from "react-icons/ri";
 
-
 const GIS = () => {
+  const [data, setData] = useState([]);
+  const customIcon = new Icon({
+    iconUrl: "https://cdn-icons-png.flaticon.com/128/684/684908.png",
+    // iconUrl: require("./../placeholder.png"),
+    iconSize: [38, 38],
+  });
 
-    const [data, setData] = useState([])
-    const customIcon = new Icon({
-        iconUrl: "https://cdn-icons-png.flaticon.com/128/684/684908.png",
-        // iconUrl: require("./../placeholder.png"),
-        iconSize: [38, 38] 
-    });
+  const Map = () => {
+    const map = useMap();
+    map.setView([28.7041, 77.1025], 5);
+    return null;
+  };
 
-    const Map = () => {
-        const map = useMap()
-        map.setView([28.7041, 77.1025], 5);
-        return null
+  useEffect(() => {
+    fetchData();
+  }, []);
+  const fetchData = async () => {
+    try {
+      const response = await fetch(
+        "http://localhost:5000/api/v1/getAllProjects"
+      );
+      const data = await response.json();
+      console.log(data.projects);
+      setData(data.projects);
+    } catch (error) {
+      console.error("Error:", error.message);
     }
+  };
 
-    useEffect(() => {
-        fetchData();
-    }, [])
-    const fetchData = async() =>{
-        try{
-            const response = await fetch('http://localhost:5000/api/v1/getAllProjects');
-            const data = await response.json();
-            console.log(data.projects);
-            setData(data.projects)
-        }catch(error){
-            console.error("Error:", error.message)
-        }
-    }
-
-    return (
-        <div className='h-screen top-0'>
-            <MapContainer className='h-screen' 
-                center={[20.8866, 79.8522]} 
-                zoom={5.49} 
-                fullscreenControl={true} 
-                touchZoom={true} 
-                scrollWheelZoom={true}
-            >
-                <TileLayer
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
-                {data.map((project) => (
+  return (
+    <div className="h-screen top-0">
+      <MapContainer
+        className="h-screen"
+        center={[20.8866, 79.8522]}
+        zoom={5.49}
+        fullscreenControl={true}
+        touchZoom={true}
+        scrollWheelZoom={true}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {data.map((project) => (
                     <Marker key={project._id} position={project.location.coordinates} icon={customIcon}>
                         <Popup>
                             This is {project.name} <br /> 
                             Status: {project.status} <br /> 
-                            <a href={`http://localhost:3000/${project._id}`} target="_blank"> 
+                            <a href={`http://localhost:3001/${project._id}`} target="_blank"> 
                             Know more  </a> 
                         </Popup>
                     </Marker>
                 ))}
-            </MapContainer>
-        </div>
-    );
-}
+      </MapContainer>
+    </div>
+  );
+};
 export default GIS;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -158,11 +158,12 @@ const Home = () => {
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d46288.90455899588!2d75.67323739752513!3d26.914251186648098!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x396c4c9b9a59a5e1%3A0xd76745ca401b0df4!2sDOP%20FILM%20CITY%20JAIPUR!5e0!3m2!1sen!2sin!4v1702907486468!5m2!1sen!2sin"
                     width="230"
                     height="230"
-                    style={{ border: '0' }}  // Use curly braces for inline styles in React
+                    style={{ border: '0' }}  
                     allowFullScreen=""
                     className='rounded-md'
                     loading="lazy"
                     referrerPolicy="no-referrer-when-downgrade"
+                    title='pie'
                 ></iframe>
 
             </div>
@@ -205,4 +206,6 @@ const Home = () => {
         </div>
     )
 }
+
+export default Home
 
